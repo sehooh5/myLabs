7fe4c8805d5f4b4768d9cbe0d7b9fb511745cbe2
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-12-07 17:45:22 +0900

    update c++ with python

--- skill/mylib.cpp
+++ skill/mylib.cpp
@@ -1,7 +1,44 @@
-#include <cstdio>
+// file : c_module_main.cpp
 
-extern "C"{
-int mul(int a,int b){
-return a*b;
-}
+#define EXPORT                       // 리눅스의 경우 별다른 지정자가 필요치 않다
+#endif
+
+#include <vector>
+#include <numeric>
+
+extern "C"
+{
+    // 1. int 타입 인자를 받고, int 타입을 리턴하는 예
+    EXPORT int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // 2. out 파라메터로 포인터를 사용하는 예
+    EXPORT void sub(double a, double b, double* result)
+    {
+        *result = a - b;
+    }
+
+    // 3. 배열 파라메터를 사용하는 예
+    EXPORT int accumulate(int* input, int size)
+    {
+        std::vector<int> v(input, input + size);
+        int result = std::accumulate(v.begin(), v.end(), 0u);
+        return result;
+    }
+
+    struct Rect
+    {
+        int x;
+        int y;
+        int width;
+        int height;
+    };
+
+    // 4. 구조체 파라메터를 사용하는 예
+    EXPORT int getarea(Rect* r)
+    {
+        return r->width * r->height;
+    }
 }
--- skill/mylib.py
+++ skill/mylib.py
@@ -1,5 +1,62 @@
-import ctypes
+# -*- coding: utf-8 -*-
+import ctypes  # 파이썬 extension을 사용하기 위한 모듈
+import platform  # 파이썬 아키텍처를 확인하기 위한 모듈
 
-a = ctypes.CDLL("./mylib.so")
-mul = a.mul
-print(mul(11,10))
+### 모듈 로드 ###
+if 'Windows' == platform.system():  # 윈도우 운영체제에서 c 모듈 로드
+    path = './x64/Debug/c_module.dll'
+    c_module = ctypes.windll.LoadLibrary(path)
+elif 'Linux' == platform.system():  # 리눅스 운영체제에서 c 모듈 로드
+    path = "./libc_module.so"
+    c_module = ctypes.cdll.LoadLibrary(path)
+else:
+    raise OSError()
+### ###
+
+
+# 1. int 타입 인자를 받고, int 타입을 리턴하는 예
+add = c_module.add
+add.argtypes = (ctypes.c_int, ctypes.c_int)
+add.restype = ctypes.c_int
+
+res = add(1, 2)
+print(res)
+
+# 2. out 파라메터로 포인터를 사용하는 예
+sub = c_module.sub
+sub.argtypes = (ctypes.c_double, ctypes.c_double, ctypes.POINTER(ctypes.c_double))
+sub.restype = None
+outparam = ctypes.c_double()
+
+sub(3.2, 2.2, outparam)
+print(outparam.value)
+
+# 3. 배열 파라메터를 사용하는 예
+accumulate = c_module.accumulate
+accumulate.argtypes = (ctypes.POINTER(ctypes.c_int), ctypes.c_int)
+accumulate.restype = ctypes.c_int
+
+s = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr = (ctypes.c_int * len(s))(*s)
+
+res = accumulate(arr, len(s))
+print(res)
+
+
+# 4. 구조체 파라메터를 사용하는 예
+class Rect(ctypes.Structure):
+    _fields_ = [
+        ('x', ctypes.c_int),
+        ('y', ctypes.c_int),
+        ('width', ctypes.c_int),
+        ('height', ctypes.c_int)
+    ]
+
+
+getarea = c_module.getarea
+getarea.argtypes = ctypes.POINTER(Rect),
+getarea.restype = ctypes.c_int
+
+r = Rect(0, 0, 5, 10)
+res = getarea(r)
+print(res)
